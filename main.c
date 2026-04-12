bfd7cd9a4755aed8fbc6eb9c24582ec6541e0eeb
4KIL0 <123417481+4KIL0@users.noreply.github.com>
2026-04-12 19:20:17 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,65 @@
+/* LIBRARIES */
+#include <stdio.h>
+#include <stdbool.h>
+#include <sys/unistd.h>
+#include "hardware/pwm.h"
+#include "pico/stdlib.h"
+#include "pico/util/queue.h"
+
+/* CONSTANTS */
+    // ISR CONSTANTS
+    #define DEBOUNCE_MS 300
+
+/* FUNCTION DECLARATIONS */
+
+    // MAIN FUNCTIONS
+
+    // ISR
+
+    // UTILITIES
+
+/* GLOBAL VARIABLES */
+
+    // ISR TIMEOUTS
+    static absolute_time_t last_press_time;
+
+void irq_rot_sw(uint gpio, uint32_t events);
+
+
+/* SYSTEM QUEUES */
+static queue_t event_queue;
+
+int main() {
+    /* SYSTEM VARIABLES */
+    bool isCalibrated = false;
+
+    while (true)
+    {
+        while (queue_try_remove() && !isCalibrated)
+        {
+
+            isCalibrated = true;
+        }
+
+        while (queue_try_remove() && isCalibrated)
+        {
+
+        }
+
+    }
+}
+
+/* ISRS */
+
+    // ROTARY ENCODER SW
+    void irq_rot_sw(uint gpio, uint32_t events)
+    {
+        absolute_time_t start_time = get_absolute_time();
+
+        if (absolute_time_diff_us(last_press_time, start_time) > DEBOUNCE_MS * 1000)
+        {
+            last_press_time = start_time;
+
+            queue_try_add()
+        }
+    }
